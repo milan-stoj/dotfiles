b5cbb5a39c551ffb3883fcab1d040c5d697fccb3
Stojanovic, Milan <milanstoj94@gmail.com>
2025-04-16 15:54:00 -0500

    feat: sync

--- nvim/lua/plugins/obsidian.lua
+++ nvim/lua/plugins/obsidian.lua
@@ -36,11 +36,36 @@ return {
       folder = 'templates',
     },
 
-    new_notes_location = '100-inbox',
+    notes_subdir = '100-inbox',
+
+    new_notes_location = 'notes-subdir',
 
     daily_notes = {
       folder = '000-daily',
       default_tags = { 'daily' },
+      template = 'daily',
     },
+
+    -- Optional, customize how note IDs are generated given an optional title.
+    ---@param title string|?
+    ---@return string
+    note_id_func = function(title)
+      -- Create note IDs in a Zettelkasten format with a timestamp and a suffix.
+      -- In this case a note with the title 'My new note' will be given an ID that looks
+      -- like '1657296016-my-new-note', and therefore the file name '1657296016-my-new-note.md'
+      local suffix = ''
+      if title ~= nil then
+        -- If title is given, transform it into valid file name.
+        suffix = title:gsub(' ', '-'):gsub('[^A-Za-z0-9-]', ''):lower()
+      else
+        -- If title is nil, just add 4 random uppercase letters to the suffix.
+        for _ = 1, 4 do
+          suffix = suffix .. string.char(math.random(65, 90))
+        end
+      end
+      local t = os.date '*t'
+      local timestamp = string.format('%04d-%02d-%02d-%02d%02d', t.year, t.month, t.day, t.hour, t.min)
+      return timestamp .. '-' .. suffix
+    end,
   },
 }

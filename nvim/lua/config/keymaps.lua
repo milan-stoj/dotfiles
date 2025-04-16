d6907f6edf94dcda36434adf51be002134cab6d6
Stojanovic, Milan <milanstoj94@gmail.com>
2025-04-16 17:35:52 -0500

    feat: sync config

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -35,3 +35,48 @@ vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper win
 -- vim.keymap.set("n", "<C-S-l>", "<C-w>L", { desc = "Move window to the right" })
 -- vim.keymap.set("n", "<C-S-j>", "<C-w>J", { desc = "Move window to the lower" })
 -- vim.keymap.set("n", "<C-S-k>", "<C-w>K", { desc = "Move window to the upper" })
+
+-- Keymap to open the index.md file in Obsidian vault
+vim.keymap.set('n', '<leader>oi', function()
+  vim.cmd 'edit ~/Obsidian/pkm-vault/300-maps/index.md'
+end, { desc = 'Open Obsidian index file' })
+
+-- Keymap to open today's daily note in Obsidian
+vim.keymap.set('n', '<leader>odt', function()
+  -- Load the obsidian.nvim plugin using lazy.nvim
+  require('lazy').load { plugins = { 'obsidian.nvim' } }
+  -- Run the ObsidianToday command
+  vim.cmd 'ObsidianToday'
+end, { desc = "Open today's daily note in Obsidian" })
+
+-- Keymap to open tomorrows daily not in buffer
+vim.keymap.set('n', '<leader>odm', function()
+  -- Load the obsidian.nvim plugin using lazy.nvim
+  require('lazy').load { plugins = { 'obsidian.nvim' } }
+  -- Run the ObsidianTomorrow command
+  vim.cmd 'ObsidianTomorrow'
+end, { desc = "Open tomorrow's daily note in Obsidian" })
+
+-- Keymap to open yesterdays daily note in buffer
+vim.keymap.set('n', '<leader>ody', function()
+  -- Load the obsidian.nvim plugin using lazy.nvim
+  require('lazy').load { plugins = { 'obsidian.nvim' } }
+  -- Run the ObsidianYesterday command
+  vim.cmd 'ObsidianYesterday'
+end, { desc = "Open yesterday's daily note in Obsidian" })
+
+-- keymap to sync the vault with git
+vim.keymap.set('n', '<leader>ovs', function()
+  local vault_path = os.getenv 'HOME' .. '/Obsidian/pkm-vault'
+  local handle = io.popen "date '+%Y-%m-%d %H:%M:%S'"
+  local datetime = handle:read('*a'):gsub('\n', '')
+  handle:close()
+  local commit_msg = 'work vault backup: ' .. datetime
+  local function run_git(cmd)
+    vim.fn.system('cd ' .. vault_path .. ' && ' .. cmd)
+  end
+  run_git 'git add .'
+  run_git("git commit -m '" .. commit_msg .. "'")
+  run_git 'git push'
+  print('Vault synced: ' .. commit_msg)
+end, { desc = 'Sync Obsidian vault with git', silent = false })

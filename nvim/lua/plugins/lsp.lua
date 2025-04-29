e55f213452331508f6c0b064b2dd4b68cc2e216e
Stojanovic, Milan <milanstoj94@gmail.com>
2025-04-28 19:03:47 -0500

    sync workspace

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -202,7 +202,7 @@ return {
       --    https://github.com/pmizio/typescript-tools.nvim
       --
       -- But for many setups, the LSP (`ts_ls`) will work just fine
-      -- ts_ls = {},
+      ts_ls = {},
       cssls = {},
       html = {},
       jdtls = {},
@@ -212,7 +212,6 @@ return {
         filetypes = { 'sql' },
         root_markers = { 'postgrestools.jsonc' },
       },
-
       lua_ls = {
         -- cmd = { ... },
         -- filetypes = { ... },

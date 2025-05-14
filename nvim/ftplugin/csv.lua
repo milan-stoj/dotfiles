ee383632e8c69695930cef33eaeb83de7e32c398
Stojanovic, Milan <milanstoj94@gmail.com>
2025-05-14 15:35:37 -0500

    feat: sync

---
+++ nvim/ftplugin/csv.lua
@@ -0,0 +1,1 @@
+require('csvview').toggle()
--- nvim/lua/plugins/render-markdown.lua
+++ nvim/lua/plugins/render-markdown.lua
@@ -2,16 +2,18 @@ return {
   -- Make sure to set this up properly if you have lazy=true
   'MeanderingProgrammer/render-markdown.nvim',
   opts = {
-    file_types = { 'markdown', 'Avante' },
+    file_types = { 'markdown' },
     heading = {
       position = 'inline',
     },
     indent = {
       enabled = true,
+      render_modes = true,
+      highlight = 'RenderMarkdownIndent',
     },
     quote = {
       repeat_linebreak = true,
     },
   },
-  ft = { 'markdown', 'Avante' },
+  ft = { 'markdown' },
 }
